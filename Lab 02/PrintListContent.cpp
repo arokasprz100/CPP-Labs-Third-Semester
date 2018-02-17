6da4db4fab8254b46c1b292cc8c90f4bddda77e7
Arkadiusz Kasprzak <36389232+arokasprz100@users.noreply.github.com>
2018-02-17 15:51:33 +0100

    Update PrintListContent.cpp

--- Lab 02/PrintListContent.cpp
+++ Lab 02/PrintListContent.cpp
@@ -5,26 +5,19 @@
 void PrintListContent (int numberOfStudents, char **wordsList)
 {
 	for (int i=0; i<numberOfStudents; i++)
-	{
 		std::cout<<wordsList[i]<<std::endl;
-	}
 
 }
 
 void PrintListContent (int numberOfStudents, int *yearsList)
 {
 	for (int i=0; i<numberOfStudents; i++)
-	{
 		std::cout<<yearsList[i]<<std::endl;
-	}
 
 }
 
 void PrintListContent (int numberOfStudents, char** namesList, char **surnamesList, int* yearsList)
 {
 	for (int i=0; i<numberOfStudents; i++)
-	{
 		std::cout<<surnamesList[i]<<", "<<namesList[i]<<" - year "<<yearsList[i]<<std::endl;
-
-	}
 }
