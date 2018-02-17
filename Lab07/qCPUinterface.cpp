11410412749094623ac39571c1f2ca33a700f66f
Arkadiusz Kasprzak <36389232+arokasprz100@users.noreply.github.com>
2018-02-17 18:58:39 +0100

    Update qCPUinterface.cpp

--- Lab07/qCPUinterface.cpp
+++ Lab07/qCPUinterface.cpp
@@ -1,40 +1,37 @@
 #include "qCPUinterface.h"
 
 ///////////////////////////////////////////////////////////////////////////
-void qInterface(const char* m_Method, const char* m_qName, int m_NqBit, int m_nExp){
-
-    using namespace std;
-	if ( !strcmp(m_Method,"Add") )     	  PARAMETER = 1;
-	if ( !strcmp(m_Method,"Redefine") )   PARAMETER = 2;
-switch (PARAMETER)
+void qInterface(const char* m_Method, const char* m_qName, int m_NqBit, int m_nExp)
 {
-case 1:
-    {
-        if (MACHINE==0)
-        {
-            cout<<">>> qInterface:: adding first qCPU"<<endl;
-            MACHINE = new TqCPU [1];
-            NCPU = 1;
-            qCPU_alloc(m_qName, m_NqBit, m_nExp, &MACHINE[0]);
-            break;
+    	using namespace std;
+	if ( !strcmp(m_Method,"Add") )     	PARAMETER = 1;
+	if ( !strcmp(m_Method,"Redefine") )   	PARAMETER = 2;
+	switch (PARAMETER)
+	{
+	case 1:
+    	{
+        	if (MACHINE==0)
+        	{
+            		cout<<">>> qInterface:: adding first qCPU"<<endl;
+            		MACHINE = new TqCPU [1];
+            		NCPU = 1;
+            		qCPU_alloc(m_qName, m_NqBit, m_nExp, &MACHINE[0]);
+            		break;
+        	}
+        	else
+        	{
+            		cout<<">>> qInterface:: adding new qCPU"<<endl;
+            		AddCPU(m_qName, m_NqBit, m_nExp);
+            		break;
+        	}
+        	break;
+    	}
+	case 2:
+    	{
+        	cout<<">>> qInterface:: redefining qCPU"<<endl;
+        	break;
 
-        }
-        else
-        {
-            cout<<">>> qInterface:: adding new qCPU"<<endl;
-            AddCPU(m_qName, m_NqBit, m_nExp);
-            break;
-
-        }
-        break;
-
-    }
-case 2:
-    {
-        cout<<">>> qInterface:: redefining qCPU"<<endl;
-        break;
-
-    }
+    	}
 
 
 	// Pseudokod:
@@ -47,23 +44,23 @@ case 2:
 	// m_Method = "Redefine" : wydrukuj tylko komunikat
 
 	// Wydrukuj komunikat dla nieznanej m_Method
-default:
-    {
+	default:
+    	{
 
-        cout<<endl;
-        cout<<">>> qInterface::Unknown method, use one of the following: \n> 'Add' add new CPU to the Machine \n> 'Redefine' redefine the existing CPU in the Machine"<<endl;
-        cout<<endl;
-        break;
-
+        	cout<<endl;
+        	cout<<">>> qInterface::Unknown method, use one of the following: \n> 'Add' add new CPU to the Machine \n> 'Redefine' redefine the existing CPU in the Machine"<<endl;
+        	cout<<endl;
+        	break;
 	}
-}
+			
+	}
 	// Uwaga: wykorzystaj: switch, new, cout, endl
 
 }
 
 ///////////////////////////////////////////////////////////////////////////
 void qInterface(const char* m_Method){
-    using namespace std;
+    	using namespace std;
 	PARAMETER = 0;
 	if ( !strcmp(m_Method,"Release") ) PARAMETER = 1;
 	if ( !strcmp(m_Method,"Evaluate") ) PARAMETER = 2;
@@ -79,46 +76,44 @@ void qInterface(const char* m_Method){
 
 	switch (PARAMETER)
 	{
-    case 1:
+    	case 1:
         {
-            for (int i=0; i<NCPU; i++)
-            {
-                delete [] MACHINE[i].cpuWord;
-            }
-            delete [] MACHINE;
-            MACHINE = 0;
-            NCPU = 0;
-            break;
+           	 for (int i=0; i<NCPU; i++)
+           	 {
+                	delete [] MACHINE[i].cpuWord;
+            	}
+            	delete [] MACHINE;
+            	MACHINE = 0;
+            	NCPU = 0;
+            	break;
         }
-    case 2:
+    	case 2:
         {
-            cout<<">>> qInterface::Machine state evaluation "<<endl;
-            for (int i=0; i<NCPU; i++)
-            {
-                Evaluate_qCPU_State(&MACHINE[i]);
-            }
-            cout<<endl;
-            break;
+            	cout<<">>> qInterface::Machine state evaluation "<<endl;
+            	for (int i=0; i<NCPU; i++)
+            	{
+                	Evaluate_qCPU_State(&MACHINE[i]);
+            	}
+            	cout<<endl;
+            	break;
         }
-    case 3:
+    	case 3:
         {
-            cout<<">>> qInterface:: Printing the Machine state "<<endl;
-            for (int i=0; i<NCPU; i++)
-            {
-                Print_qCPU_State(MACHINE[i]);
-            }
-
-            break;
+           	cout<<">>> qInterface:: Printing the Machine state "<<endl;
+            	for (int i=0; i<NCPU; i++)
+                	Print_qCPU_State(MACHINE[i]);
+      		
+            	break;
         }
-    default:
+    	default:
         {
-            cout<<endl;
-            cout<<"qInterface::Unknown method, use one of the following:"<<endl;
-            cout<<">'Evaluate' evaluate the Machine state"<<endl;
-            cout<<">'Print' printing the Machine state"<<endl;
-            cout<<">'Release' destroy the Machine"<<endl;
-            cout<<endl;
-            break;
+            	cout<<endl;
+            	cout<<"qInterface::Unknown method, use one of the following:"<<endl;
+           	 cout<<">'Evaluate' evaluate the Machine state"<<endl;
+            	cout<<">'Print' printing the Machine state"<<endl;
+            	cout<<">'Release' destroy the Machine"<<endl;
+            	cout<<endl;
+            	break;
         }
 
 
@@ -135,20 +130,17 @@ void AddCPU(const char* m_qName, int m_NqBit, int m_nExp){
 
 	TqCPU *temp = new TqCPU [NCPU];
 	for (int i=0; i<NCPU; i++)
-    {
-        temp[i]=MACHINE[i];
-    }
-    delete [] MACHINE;
-    MACHINE = new TqCPU [NCPU+1];
-    for (int i=0; i<NCPU; i++)
-    {
-        MACHINE[i] = temp[i];
-    }
-    delete [] temp;
+    	{
+        	temp[i]=MACHINE[i];
+    	}
+    	delete [] MACHINE;
+    	MACHINE = new TqCPU [NCPU+1];
+    	for (int i=0; i<NCPU; i++)
+    	{
+        	MACHINE[i] = temp[i];
+    	}
+    	delete [] temp;
 
-    qCPU_alloc (m_qName, m_NqBit, m_nExp, &MACHINE[NCPU]);
-    NCPU++;
-
-
-
+    	qCPU_alloc (m_qName, m_NqBit, m_nExp, &MACHINE[NCPU]);
+    	NCPU++;
 }
